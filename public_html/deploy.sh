90d8b9b87dc69290b8e3f302336573ad73dec4bc
sakamotomsh <sakamoto.masahiko@oss.ntt.co.jp>
2011-09-04 12:03:11 +0900

    add deploy script for project project website.
    project website provides static html files.
    See http://interdbconnect.sourceforge.net/

---
+++ public_html/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ x$1 == x"" ]
+then
+    echo "enter accountid"
+    exit 1;
+fi
+
+USERNAME=$1
+
+rsync -avz index.html $USERNAME@web.sourceforge.net:/home/project-web/interdbconnect/htdocs/index.html
+rsync -avz style.css $USERNAME@web.sourceforge.net:/home/project-web/interdbconnect/htdocs/style.css
+rsync -avz ../syncdb/doc/ $USERNAME@web.sourceforge.net:/home/project-web/interdbconnect/htdocs/syncdb/
+rsync -avz ../dblink_plus/doc/ $USERNAME@web.sourceforge.net:/home/project-web/interdbconnect/htdocs/dblink_plus/
+
