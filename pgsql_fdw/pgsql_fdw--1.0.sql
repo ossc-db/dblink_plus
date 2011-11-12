a52675672c61794340dfc38501503aadcd31f3b8
Shigeru Hanada <hanada@jupiter.(none)>
2011-11-13 02:17:39 +0900

    Remove error message for unexpected installation via psql.
    Ignoring \echo line is implemented at 9.2, so this error message should
    not be included in a script for extensions which are available for 9.1.

--- pgsql_fdw/pgsql_fdw--1.0.sql
+++ pgsql_fdw/pgsql_fdw--1.0.sql
@@ -1,8 +1,5 @@
 /* pgsql_fdw/pgsql_fdw--1.0.sql */
 
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION pgsql_fdw" to load this file. \quit
-
 CREATE FUNCTION pgsql_fdw_handler()
 RETURNS fdw_handler
 AS 'MODULE_PATHNAME'
