1cba48a23955c5241fba1e50dd3072ccf663209e
yamatattsu <yamatattsu@gmail.com>
2019-01-10 18:01:38 +0900

    Fix regression test for 90d4f203a5b5c25c697d4e229e7f2cc7c2900b34 (part 2)

--- sql/standby_postgres.sql
+++ sql/standby_postgres.sql
@@ -2,9 +2,9 @@
 BEGIN;
 SET dblink_plus.use_xa TO 'on';
 SHOW dblink_plus.use_xa;
-SELECT * FROM dblink.query('server_postgres6789012345678901234567890123456789012345678901234567890', 'SELECT * FROM dblink_tbl ORDER BY id') AS t(id integer, value text) ORDER BY id;
+SELECT * FROM dblink.query('server_postgres6789012345678901234567890123456789012345678901234567890', 'SELECT * FROM dblink.dblink_tbl ORDER BY id') AS t(id integer, value text) ORDER BY id;
 COMMIT;
 BEGIN;
 SET dblink_plus.use_xa TO 'off';
-SELECT * FROM dblink.query('server_postgres6789012345678901234567890123456789012345678901234567890', 'SELECT * FROM dblink_tbl ORDER BY id') AS t(id integer, value text) ORDER BY id;
+SELECT * FROM dblink.query('server_postgres6789012345678901234567890123456789012345678901234567890', 'SELECT * FROM dblink.dblink_tbl ORDER BY id') AS t(id integer, value text) ORDER BY id;
 COMMIT;
